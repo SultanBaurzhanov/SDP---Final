e7565ecdc1a1e7240ecab4b5b75750b94ad372e3
ParZival6677 <113224392+ParZival6677@users.noreply.github.com>
2023-11-19 01:33:39 +0600

    Update Notification.java

--- Notification.java
+++ Notification.java
@@ -1,6 +1,6 @@
 // Chain of Responsibility pattern
 interface Notification {
-    void notify(String taskId);
+    void notify(String userName, String taskId);
 }
 
 class NotificationSystem implements Notification {
@@ -10,20 +10,23 @@ class NotificationSystem implements Notification {
         this.next = next;
     }
 
-    public void notify(String taskId) {
-        System.out.println("Sending notifications for task " + taskId);
-        next.notify(taskId);
+    public void notify(String userName, String taskId) {
+        System.out.println("Sending notifications for task " + taskId + " to user " + userName);
+
+        if (next != null) {
+            next.notify(userName, taskId);
+        }
     }
 }
 
 class EmailNotification implements Notification {
-    public void notify(String taskId) {
-        System.out.println("Sending email notification for task " + taskId);
+    public void notify(String userName, String taskId) {
+        System.out.println("Sending email notification for task " + taskId + " to user " + userName);
     }
 }
 
 class SmsNotification implements Notification {
-    public void notify(String taskId) {
-        System.out.println("Sending SMS notification for task " + taskId);
+    public void notify(String userName, String taskId) {
+        System.out.println("Sending SMS notification for task " + taskId + " to user " + userName);
     }
 }
