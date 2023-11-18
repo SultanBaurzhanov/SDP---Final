8fd670922e2b011aa962343eba05fe55ff1c0b4a
ParZival6677 <113224392+ParZival6677@users.noreply.github.com>
2023-11-18 21:49:16 +0600

    Update TaskBuilder.java

--- TaskBuilder.java
+++ TaskBuilder.java
@@ -1,53 +1,43 @@
-import java.util.Date;
+import java.util.Stack;
 
-// Builder
-class TaskBuilder {
-    private String taskTitle;
-    private String taskDescription;
-    private Date dueDate;
+public class Task {
+    private int id;
+    private String name;
+    private String description;
+    private String status;
 
-    public TaskBuilder setTaskTitle(String taskTitle) {
-        this.taskTitle = taskTitle;
+    public Task(TaskBuilder taskBuilder) {
+        this.id = taskBuilder.id;
+        this.name = taskBuilder.name;
+        this.description = taskBuilder.description;
+        this.status = taskBuilder.status;
+    }
+
+    // getters and setters
+}
+
+public class TaskBuilder {
+    private int id;
+    private String name;
+    private String description;
+    private String status;
+
+    public TaskBuilder(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public TaskBuilder description(String description) {
+        this.description = description;
         return this;
     }
 
-    public TaskBuilder setTaskDescription(String taskDescription) {
-        this.taskDescription = taskDescription;
-        return this;
-    }
-
-    public TaskBuilder setDueDate(Date dueDate) {
-        this.dueDate = dueDate;
+    public TaskBuilder status(String status) {
+        this.status = status;
         return this;
     }
 
     public Task build() {
-        return new Task(taskTitle, taskDescription, dueDate);
-    }
-}
-
-// Task
-class Task {
-    private String taskTitle;
-    private String taskDescription;
-    private Date dueDate;
-
-    public Task(String taskTitle, String taskDescription, Date dueDate) {
-        this.taskTitle = taskTitle;
-        this.taskDescription = taskDescription;
-        this.dueDate = dueDate;
-    }
-
-    // Getters for task attributes
-    public String getTaskTitle() {
-        return taskTitle;
-    }
-
-    public String getTaskDescription() {
-        return taskDescription;
-    }
-
-    public Date getDueDate() {
-        return dueDate;
+        return new Task(this);
     }
 }
