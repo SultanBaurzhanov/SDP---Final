fe2012143608aa2bdccc2ea559e8d1f49d7672d7
ParZival6677 <113224392+ParZival6677@users.noreply.github.com>
2023-11-19 01:32:11 +0600

    Update Observer.java

--- Observer.java
+++ Observer.java
@@ -5,12 +5,24 @@ interface Observer {
 
 class User implements Observer {
     private String name;
+    private Notification notificationPreference;
 
     public User(String name) {
         this.name = name;
     }
 
+    public void setNotificationPreference(Notification notificationPreference) {
+        this.notificationPreference = notificationPreference;
+    }
+
     public void update(String taskId) {
         System.out.println("Dear " + name + ", task " + taskId + " has been updated.");
+        if (notificationPreference != null) {
+            notificationPreference.notify(name, taskId);
+        }
+    }
+
+    public String getName() {
+        return name;
     }
 }
