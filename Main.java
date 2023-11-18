b61320a4a7a5aaf1a206af52ec00e0aa1df97fb8
ParZival6677 <113224392+ParZival6677@users.noreply.github.com>
2023-11-19 01:57:45 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -26,7 +26,7 @@ public class Main {
 
         // Simulate task update
         task.setAssignee("Bob");
-        taskManager.createTask(task);
+        taskManager.updateTask(task);
 
         // Undo the task creation
         createTaskCommand.undo();
