1cfe92af2f7ef418e0646a5b5ec6a0a50cc670ef
ParZival6677 <113224392+ParZival6677@users.noreply.github.com>
2023-11-18 22:37:15 +0600

    Update Observer.java

--- Observer.java
+++ Observer.java
@@ -1,47 +1,16 @@
-import java.util.ArrayList;
-import java.util.List;
-
-// Observer
+// Observer pattern
 interface Observer {
-    void update(Task task);
+    void update(String taskId);
 }
 
-class TaskSubject {
-    private List<Observer> observers;
-    private Task task;
-
-    public TaskSubject() {
-        this.observers = new ArrayList<>();
-    }
-
-    public void attach(Observer observer) {
-        this.observers.add(observer);
-    }
-
-    public void detach(Observer observer) {
-        this.observers.remove(observer);
-    }
-
-    public void notifyObservers() {
-        for (Observer observer : observers) {
-            observer.update(task);
-        }
-    }
-
-    public void setTask(Task task) {
-        this.task = task;
-    }
-}
-
-class UserObserver implements Observer {
+class User implements Observer {
     private String name;
 
-    public UserObserver(String name) {
+    public User(String name) {
         this.name = name;
     }
 
-    @Override
-    public void update(Task task) {
-        System.out.println("Hello, " + name + "! Task has been updated. New details: " + task);
+    public void update(String taskId) {
+        System.out.println("Dear " + name + ", task " + taskId + " has been updated.");
     }
 }
