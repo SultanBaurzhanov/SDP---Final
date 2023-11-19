63c91fcec87382877370de74d902f4ec0062fcc1
ParZival6677 <113224392+ParZival6677@users.noreply.github.com>
2023-11-19 12:42:08 +0600

    Update Command.java

--- Command.java
+++ Command.java
@@ -2,42 +2,77 @@
 interface Command {
     void execute();
     void undo();
+    void redo();
 }
 
 class CreateTaskCommand implements Command {
     private Task task;
     private TaskManager taskManager;
+    private boolean executed;
 
     public CreateTaskCommand(Task task, TaskManager taskManager) {
         this.task = task;
         this.taskManager = taskManager;
+        this.executed = false;
     }
 
     public void execute() {
         taskManager.createTask(task);
+        executed = true;
     }
 
     public void undo() {
-        taskManager.deleteTask(task);
+        if (executed) {
+            taskManager.deleteTask(task);
+            executed = false;
+        }
+    }
+
+    public void redo() {
+        if (!executed) {
+            taskManager.createTask(task);
+            executed = true;
+        }
     }
 }
 
+
 class AssignTaskCommand implements Command {
     private String taskId;
-    private String assignee;
+    private String assigneeBefore;
+    private String assigneeAfter;
     private TaskManager taskManager;
+    private boolean executed;
 
     public AssignTaskCommand(String taskId, String assignee, TaskManager taskManager) {
         this.taskId = taskId;
-        this.assignee = assignee;
+        this.assigneeAfter = assignee;
         this.taskManager = taskManager;
+        this.executed = false;
     }
 
     public void execute() {
-        taskManager.assignTask(taskId, assignee);
+        Task task = taskManager.getTaskById(taskId);
+        if (task != null) {
+            assigneeBefore = task.getAssignee();
+            taskManager.assignTask(taskId, assigneeAfter);
+            executed = true;
+        } else {
+            System.out.println("Task " + taskId + " not found");
+        }
     }
 
     public void undo() {
-        taskManager.unassignTask(taskId);
+        if (executed) {
+            taskManager.assignTask(taskId, assigneeBefore);
+            executed = false;
+        }
+    }
+
+    public void redo() {
+        if (!executed) {
+            taskManager.assignTask(taskId, assigneeAfter);
+            executed = true;
+        }
     }
 }
